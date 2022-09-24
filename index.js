bd06101fd65802d1eb99809795760f98f9305447
Charles Ukasoanya <c_ukasoanya@yahoo.com>
2022-09-24 23:54:39 +0100

    implemented react@18 createRoot setup

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
-ReactDOM.render(
-  <h1> Hello, everyone! Welcome to our React Notes app</h1>,
-  document.getElementById('root'),
+const navbar = (
+  <nav>
+    <h1>Paddy's Cafe</h1>
+    <ul>
+      <li>Menu</li>
+      <li>About</li>
+      <li>Contact</li>
+    </ul>
+  </nav>
 );
+
+// ReactDOM.render(navbar, document.getElementById('root'));
+ReactDOM.createRoot(document.getElementById('root')).render(navbar);
