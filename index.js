fed5e212e35fdb919d4166a1ae85d7e345b91593
Charles Ukasoanya <c_ukasoanya@yahoo.com>
2022-09-25 00:17:52 +0100

    added markup for info site project

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
-const navbar = (
-  <nav>
-    <h1>Paddy's Cafe</h1>
+const page = (
+  <div>
+    <nav>
+      <img src="./react-logo.png" alt="react-logo" width="40px" />
+    </nav>
+
+    <h1>Fun facts about React.</h1>
+
     <ul>
-      <li>Menu</li>
-      <li>About</li>
-      <li>Contact</li>
+      <li>Was first released in 2013.</li>
+      <li>Was originally created by Jordan Walke</li>
+      <li>He has over 100k stars on Github.</li>
+      <li>Is maintained by Facebook</li>
+      <li>Powers thousands of enterprise apps including mobile apps.</li>
     </ul>
-  </nav>
+  </div>
 );
 
-// ReactDOM.render(navbar, document.getElementById('root'));
-ReactDOM.createRoot(document.getElementById('root')).render(navbar);
+ReactDOM.render(page, document.getElementById('root'));
